4771e0877bf0f6a1693deb9cd68d0f399e25a4c3
jhemyson sousa <jhemyson_@hotmail.com>
2018-05-08 17:37:29 -0300

    Adcionando produtos, perguntas e respostas de produtos

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product, ProductQuestion, ProductAnswer
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,6 +8,27 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'parent', 'hidden')
 
 
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+    list_filter = ['status']
+    list_display = ('id', 'name', 'short_description', 'status')
+
+
+class ProductAnswerInline(admin.StackedInline):
+    model = ProductAnswer
+    can_delete = False
+
+
+class ProductQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'question', 'status')
+
+    inlines = (ProductAnswerInline,)
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductQuestion, ProductQuestionAdmin)
+
+
 
 
--- portal/models.py
+++ portal/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     parent = models.ForeignKey('Category', null=True, blank=True, related_name='sub_category', on_delete=models.CASCADE)
     order = models.IntegerField(null=True, blank=True)
@@ -13,6 +14,64 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    user = models.OneToOneField(User)
+    categories = models.ManyToManyField(Category, blank=True, related_name='categories')
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    STATUS_CHOICES  = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Inactive")
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+
+
+class ProductQuestion(models.Model):
+    user = models.OneToOneField(User)
+    product = models.ForeignKey('Product')
+    question = models.TextField()
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Inactive")
+
+    class Meta:
+        verbose_name_plural = "Product Questions"
+
+    def __str__(self):
+        return self.question
+
+
+class ProductAnswer(models.Model):
+    user = models.OneToOneField(User)
+    product_question = models.ForeignKey(ProductQuestion)
+    answer = models.TextField()
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Inactive")
+
+    class Meta:
+        verbose_name_plural = "Product Answers"
+
+    def __str__(self):
+        return self.answer
+
+
 
 
 
